496de4e1e20f0906a030942835be62fd426a8f3e
Caio_Lins <C@iolins123>
2023-04-25 22:34:50 -0300

    Finalizando projeto alura space

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
-# Register your models here.
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda", "descricao", "categoria", "foto", "publicada")
+    list_display_links = ("id", "nome", "legenda", "descricao", "categoria", "foto")
+    search_fields = ("nome", "id")
+    list_filter = ("categoria",)
+    list_editable = ("publicada",)
+    list_per_page = 10
+
+admin.site.register(Fotografia, ListandoFotografias)
