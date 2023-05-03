56d741a1dfd6348b558e1b9310aa0b3794735b2c
Caio_Lins <C@iolins123>
2023-05-03 19:29:49 -0300

    resolvendo problemas

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+
 from galeria.models import Fotografia
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ("id", "nome", "legenda", "descricao", "categoria", "foto", "publicada")
-    list_display_links = ("id", "nome", "legenda", "descricao", "categoria", "foto")
-    search_fields = ("nome", "id")
+    list_display = ("id", "nome", "legenda", "publicada")
+    list_display_links = ("id","nome")
+    search_fields = ("nome",)
     list_filter = ("categoria",)
     list_editable = ("publicada",)
     list_per_page = 10
--- galeria/models.py
+++ galeria/models.py
@@ -1,15 +1,16 @@
 from django.db import models
+
 from datetime import datetime
 
 class Fotografia(models.Model):
 
     OPCOES_CATEGORIA = [
-        ("NEBULOSA", "Nebulosa"),
-        ("ESTRELA", "Estrela"),
-        ("GALÁXIA", "Galáxia"),
-        ("PLANETA", "Planeta")
+        ("NEBULOSA","Nebulosa"),
+        ("ESTRELA","Estrela"),
+        ("GALÁXIA","Galáxia"),
+        ("PLANETA","Planeta"),
     ]
-    
+
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
     categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default='')
@@ -20,4 +21,3 @@ class Fotografia(models.Model):
 
     def __str__(self):
         return self.nome
-
